61e0530b7589dae370f4ea33351012fede94e8df
XZaB1 <126421383+XZaB1@users.noreply.github.com>
2023-05-08 01:58:36 +0300

    Add files via upload

---
+++ LR3/hotel1/auth/index.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+if ($_SESSION['user']) {
+    header('Location: profile.php');
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация и регистрация</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+<!-- Форма авторизации -->
+
+    <form action="vendor/signin.php" method="post">
+        <label>Логин</label>
+        <input type="text" name="login" placeholder="Введите свой логин">
+        <label>Пароль</label>
+        <input type="password" name="password" placeholder="Введите пароль">
+        <button type="submit">Войти</button>
+        <p>
+            У вас нет аккаунта? - <a href="/auth/register.php">зарегистрируйтесь</a>!
+        </p>
+        <a href="/" class="logout">Выход на сайт</a>
+        <?php
+            if ($_SESSION['message']) {
+                echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
+            }
+            unset($_SESSION['message']);
+        ?>
+    </form>
+
+</body>
+</html>
